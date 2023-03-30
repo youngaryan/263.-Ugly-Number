f11a65eaa2750c4f88270698ea01c663e7cc0b6b
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-30 11:52:07 +0100

    Done

--- Solution.java
+++ Solution.java
@@ -1,46 +1,19 @@
-import java.util.*;
 
 class Solution {
     public boolean isUgly(int n) {
-        int count = 0;
-        List<Integer> list = new ArrayList<>();
-        int [] allowed = new int[]{1,2,3,5};
+        int [] allowed = new int[]{2,3,5};
 
-        if (n <= 0)
+        if (n<=0)
             return false;
 
-        for (int i = 1; i <= n ; i++) {
-             if (n % i == 0 && checkForPrime(i))
-                 list.add(i);
-        }
-        Collections.sort(list);
-
-        for (int j : allowed) {
-            if (list.contains(j))
-                count++;
-        }
-        return count == list.size();
-
-    }
-
-
-    static boolean checkForPrime(int inputNumber) {
-        boolean isItPrime = true;
-
-        if (inputNumber < 1) {
-            isItPrime = false;
-
-            return isItPrime;
-        } else {
-            for (int i = 2; i <= inputNumber / 2; i++) {
-                if ((inputNumber % i) == 0) {
-                    isItPrime = false;
-
-                    break;
-                }
+        for (int factor: allowed ){
+            while (n%factor == 0){
+                n =n/factor;
             }
-
-            return isItPrime;
         }
+        return n==1;
     }
+
+
+
 }
